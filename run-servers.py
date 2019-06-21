1824b80c390e9e9fb541da2f2224984d45ab6408
Akshay Narayan <akshayn@mit.edu>
2019-06-21 18:20:26 +0000

    use etgServer in script

--- run-servers.py
+++ run-servers.py
@@ -8,7 +8,7 @@ start_port = int(sys.argv[1])
 num_procs = int(sys.argv[2])
 cong_alg = sys.argv[3]
 
-procs = [sh.Popen(shlex.split('./bin/server -t {} -p {}'.format(cong_alg, start_port + i))) for i in range(num_procs)]
+procs = [sh.Popen(shlex.split('./bin/etgServer -t {} -p {}'.format(cong_alg, start_port + i))) for i in range(num_procs)]
 for p in procs:
     p.wait()
 
